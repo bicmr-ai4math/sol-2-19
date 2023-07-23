7655d671e4a61ee372003070a1f5a00fa4cf2689
idesty <107295593+idesty@users.noreply.github.com>
2023-07-23 23:00:28 +0800

    Update Pre4.lean

---
+++ Pre4.lean
@@ -0,0 +1,152 @@
+import Mathlib.Tactic
+import Mathlib.NumberTheory.LegendreSymbol.GaussEisensteinLemmas
+
+import Mathlib.GroupTheory.SpecificGroups.Cyclic
+
+import Mathlib.Data.Real.Basic
+
+#check subgroup_units_cyclic
+#check IsCyclic.card_pow_eq_one_le
+
+
+-- Z/nZ
+variable (n : ℕ)
+#check ZMod n
+
+-- the type/set of solutions to an equation in a finite type/set
+def sol_sum2squares
+    (p : ℕ) [Fact (Nat.Prime p)] : Finset (ZMod p × ZMod p) :=
+  Finset.univ.filter (fun (x, y) ↦ x^2 + y^2 = 1)
+-- def my_sol
+--     {α : Type} [DecidableEq α] [Fintype α] (eq : α → Prop) : Finset α :=
+--   Finset.filter eq Finset.univ
+variable (p : ℕ) [Fact (Nat.Prime p)]
+#check Finset.card (sol_sum2squares p)
+
+def sol_1square
+    (p : ℕ) [Fact (Nat.Prime p)] (a : ZMod p) : Finset (ZMod p) :=
+  Finset.univ.filter (fun x ↦ x^2 = a)
+
+def sol_dif2squares_unit
+    (p : ℕ) [Fact (Nat.Prime p)] : Finset (ZMod p × ZMod p) :=
+  Finset.univ.filter (fun (x, y) ↦ x^2 - y^2 = 1)
+
+
+
+--For Lemma 2
+def sol_mul_unit
+    (p : ℕ) [Fact (Nat.Prime p)] : Finset (ZMod p × ZMod p) :=
+  Finset.univ.filter (fun (x, y) ↦ x * y = 1)
+--For Lemma 2
+
+open Function
+
+-- lemma h_to : ∀ x y : ZMod p, (x^2 - y^2 = 1 ↔ (x + y) * (x - y) = 1) := by
+--   intro x y
+--   constructor
+--   · ring
+--     exact fun a ↦ a
+--   ring
+--   exact fun a ↦ a
+
+#check Finset.mem_filter.mpr
+
+-- trans (x, y) -> (x+y, x-y) into set theorical function
+def f' : sol_dif2squares_unit p -> sol_mul_unit p := by
+  rintro ⟨⟨x, y⟩, h⟩
+  constructor
+  swap
+  constructor
+  exact x + y
+  exact x - y
+  apply Finset.mem_filter.mpr
+  constructor
+  exact Finset.mem_univ (x + y, x - y)
+  simp
+  have ⟨fst, snd⟩ := Finset.mem_filter.mp h
+  simp at snd
+  ring
+  exact snd
+
+def f'_bi : Bijective (f' p) := by
+  constructor
+  · intro a0 a1
+    have h0 := f' p a0
+    rcases h0 with ⟨h0_val, h0_p⟩
+    have h1 := f' p a1
+    rcases h1 with ⟨h1_val, h1_p⟩
+    intro h
+    sorry
+  sorry
+
+
+
+
+
+
+example (p : ℕ) [Fact (Nat.Prime p)] : Bijective (fun x => x ∈ sol_dif2squares_unit p -> x ∈ sol_mul_unit p) := by
+  constructor
+  · unfold Injective
+    intro a1 a2
+    simp
+    intro hP
+    unfold sol_dif2squares_unit at hP
+    sorry
+  sorry
+
+
+theorem card_sol_1square
+    (p : ℕ) [Fact p.Prime] (a : ZMod p) :
+  (sol_1square p a).card = 1 + legendreSym p a := by
+  by_cases a = 0
+  · have : legendreSym p a = 0 := by
+      apply (legendreSym.eq_zero_iff p a).mpr
+      simp
+      exact h
+    rw [this, h]; simp
+    have : ∀(x : ZMod p), x ∈ sol_1square p 0 ↔ x = 0 := by
+      -- dsimp
+      -- intro x
+      -- rw [Finset.mem_filter] -- why can't rw here?
+      intro x
+      constructor
+      · intro h
+        have : x^2 = 0 := (Finset.mem_filter.mp h).right
+        exact sq_eq_zero_iff.mp this
+      sorry
+    sorry
+  sorry
+
+
+
+
+lemma card_sol_dif2squares_unit
+    (p : ℕ) [Fact (Nat.Prime p)] (a b : ZMod p) : (sol_dif2squares_unit p).card = p - 1 := by
+    -- have : ∀ a₁ a₂ b₁ b₂ : ZMod p, ((a₁ = a₂ ∧ b₁ = b₂) ↔ (a₁ + b₁ = a₂ + b₂ ∧ a₁ - b₁ = a₂ - b₂)) := by
+    --   intro a₁ a₂ b₁ b₂
+    --   constructor
+    --   ·
+
+
+
+    -- have : ∀ x : ZMod p, ∃! y : ZMod p, x^2 - y^2 = 1 := by
+    --   intro x
+    --   have : Bijective (fun x ↦ (x, y)) := by
+    --     have : ∃! y : ZMod p, y^2 = x^2 - 1 := by
+    --       -- apply card_sol_1square p (x^2 - 1)
+    --       sorry
+    --     sorry
+    --   sorry
+    -- sorry
+
+
+      -- sorry
+    -- sorry
+
+
+
+
+theorem card_sol_sum2squares
+    (p : ℕ) [Fact (Nat.Prime p)] :
+  (sol_sum2squares p).card = 1 - (-1 : ℤ)^((p-1)/2) := by
+  sorry
